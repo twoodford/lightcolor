c09427644c0460144b4e6bc264d7693d0c6df690
Tim Woodford <timthewoodford@gmail.com>
2016-04-15 11:40:02 -0400

    Add a Apache license

--- commonlib/color.h
+++ commonlib/color.h
@@ -1,7 +1,7 @@
 /*
  * color.h
  * Color handling for LED strips
- * 
+ *
  * Copyright (C) 2016 Tim Woodford.  All rights reserved.
  *
  */
@@ -10,6 +10,7 @@
 #ifndef _CL_COLOR_H_
 #define _CL_COLOR_H_
 
+// RGB Color for interchange with Arduino
 typedef struct color_rgb {
     uint8_t r;
     uint8_t g;
